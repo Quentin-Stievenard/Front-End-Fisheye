1bea78c1eec1695f15152d7ef91b92fe9e1221ba
Quentin Stievenard <qstievenard@gmail.com>
2021-11-26 11:35:13 +0100

    :construction: Detail Photographers

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,82 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { name, portrait, city, country, price, tagline } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    const location = document.createElement("p");
+    location.textContent = city + ", " + country;
+    location.className = "location";
+    const slogan = document.createElement("p");
+    slogan.textContent = tagline;
+    slogan.className = "slogan";
+    const prices = document.createElement("p");
+    prices.textContent = price + "/jour";
+    prices.className = "prices";
+
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(location);
+    article.appendChild(slogan);
+    article.appendChild(prices);
+    return article;
+  }
+  return { name, picture, city, country, price, tagline, getUserCardDOM };
+}
+
+function photographerDetails(data) {
+  const { name, portrait, city, country, price, tagline } = data;
+
+  const picture = `assets/photographers/${portrait}`;
+
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    const location = document.createElement("p");
+    location.textContent = city + ", " + country;
+    location.className = "location";
+    const slogan = document.createElement("p");
+    slogan.textContent = tagline;
+    slogan.className = "slogan";
+
+    article.appendChild(h2);
+    article.appendChild(location);
+    article.appendChild(slogan);
+
+    return article;
+  }
+  return { name, picture, city, country, price, tagline, getUserCardDOM };
+}
+
+function photographerMedia(data) {
+  const { date, image, likes, price, title } = data;
+
+  const picture = `assets/photographers/${image}`;
+
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    const location = document.createElement("p");
+    location.textContent = city + ", " + country;
+    location.className = "location";
+    const slogan = document.createElement("p");
+    slogan.textContent = tagline;
+    slogan.className = "slogan";
+
+    article.appendChild(h2);
+    article.appendChild(location);
+    article.appendChild(slogan);
+
+    return article;
+  }
+  return { name, picture, city, country, price, tagline, getUserCardDOM };
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,41 @@
 //Mettre le code JavaScript lié à la page photographer.html
+async function getPhotographers() {
+  fetch("../../data/photographers.json")
+    .then((response) => response.json())
+    .then((data) => displayPhotographers(data.photographers[1]))
+    .then(() => {
+      const picture = "assets/photographers/MimiKeel.jpg";
+      const avatar = document.querySelector(".photograph-img");
+      avatar.setAttribute("src", picture);
+    });
+}
+
+async function getMedias() {
+  fetch("../../data/photographers.json")
+    .then((response) => response.json())
+    .then((data) => console.log(data.media));
+}
+
+async function displayPhotographers(photographer) {
+  const photographersSection = document.querySelector(
+    ".photograph-information"
+  );
+  const photographerModel = photographerDetails(photographer);
+  const userCardDOM = photographerModel.getUserCardDOM();
+  photographersSection.appendChild(userCardDOM);
+}
+
+async function displayMedias(medias) {
+  const photographersMedia = document.querySelector(".photograph-media");
+  medias.forEach((media) => {
+    const photographerModel = photographerMedia(media);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersMedia.appendChild(userCardDOM);
+  });
+}
+async function init() {
+  getPhotographers();
+  getMedias();
+}
+
+init();
